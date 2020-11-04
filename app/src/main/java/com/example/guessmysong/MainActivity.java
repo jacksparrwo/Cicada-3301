76608ddff0735a95af8f50cc93b4c9e53035ca6a
Nagy Bianka <nagy_bianca99@yahoo.com>
2020-11-04 16:48:21 +0200

    Main Page added and connected to Login button

--- app/src/main/java/com/example/guessmysong/MainActivity.java
+++ app/src/main/java/com/example/guessmysong/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.guessmysong;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        openMainPage();
+    }
+
+    public  void openMainPage(){
+        Intent intent = new Intent(this, MainPage.class);
+        startActivity(intent);
     }
 }
