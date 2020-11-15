7d7b21bfaa59c5fe682d91fc4210c9836e1b876d
Nagy Bianka <nagy_bianca99@yahoo.com>
2020-11-15 16:28:49 +0200

    Facebook Login Functional

--- app/src/main/java/com/example/guessmysong/MainActivity.java
+++ app/src/main/java/com/example/guessmysong/MainActivity.java
@@ -2,15 +2,16 @@ package com.example.guessmysong;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 import com.bumptech.glide.request.RequestOptions;
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
@@ -19,11 +20,14 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.Arrays;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -41,93 +45,77 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         loginButton = findViewById(R.id.facebookLogin);
-        txtName = findViewById(R.id.username);
-        txtPassword= findViewById(R.id.password);
+
         circleImageView = findViewById(R.id.facebookLogo);
 
         callbackManager = CallbackManager.Factory.create();
-        checkLoginStatus();
+
+        loginButton.setPermissions(Arrays.asList("user_gender, user_friends"));
+
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
 
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onSuccess(LoginResult loginResult) {
+            public void onSuccess(LoginResult loginResult) { //successful login
+                Log.d("Successful", "Login successful");
+                startActivity(new Intent(MainActivity.this, MainPage.class));
 
             }
 
             @Override
-            public void onCancel() {
+            public void onCancel() {  //canceled login
 
             }
 
             @Override
-            public void onError(FacebookException error) {
+            public void onError(FacebookException error) { //error on login
 
             }
         });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        callbackManager.onActivityResult(requestCode,resultCode,data);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+
+        GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                Log.d("Demo", object.toString());
+                try{
+                    String username = object.getString("name");
+                    //txtName.setText(username);
+                } catch(JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Bundle bundle = new Bundle();
+        bundle.putString("fields", "gender, name, id, first_name, last_name");
+
+
+        graphRequest.setParameters(bundle);
+        graphRequest.executeAsync();
     }
 
-    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+    AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
         @Override
         protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if(accessTokenTracker == null){
+                LoginManager.getInstance().logOut();
 
-            if(currentAccessToken == null){
-                txtName.setText("");
-                txtPassword.setText("");
-                circleImageView.setImageResource(0);
-                Toast.makeText(MainActivity.this,"User logged out", Toast.LENGTH_LONG).show();
-            }
-            else{
-                openMainPage(currentAccessToken);
             }
         }
     };
 
-
-    public  void openMainPage(AccessToken newAccessToken){
-
-        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
-            @Override
-            public void onCompleted(JSONObject object, GraphResponse response) {
-                try {
-                    String first_name = object.getString("first_name");
-                    String last_name = object.getString("last_name");
-                    String password = object.getString("password");
-                    String id = object.getString("id");
-
-                    txtName.setText(first_name + " "+ last_name);
-                    txtPassword.setText(password);
-
-                    RequestOptions requestOptions = new RequestOptions();
-                    requestOptions.dontAnimate();
-
-
-
-                } catch (JSONException e){
-                    e.printStackTrace();
-                }
-
-
-            }
-        });
-
-        Bundle parameters = new Bundle();
-        parameters.putString("fields","first_name, last_name, password, id");
-        request.setParameters(parameters);
-        request.executeAsync();
-
-
-    }
-
-    private void checkLoginStatus(){
-        if(AccessToken.getCurrentAccessToken()!=null){
-            openMainPage(AccessToken.getCurrentAccessToken());
-        }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
     }
 }
--- app/src/main/java/com/example/guessmysong/MainPage.java
+++ app/src/main/java/com/example/guessmysong/MainPage.java
@@ -1,22 +1,10 @@
 package com.example.guessmysong;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-
-import com.facebook.AccessToken;
-import com.facebook.AccessTokenTracker;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-
-import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainPage extends AppCompatActivity {
 
@@ -27,12 +15,7 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-
-
-
     }
-
-
 
     public void onClickSettings(View view){
         openSettings();
