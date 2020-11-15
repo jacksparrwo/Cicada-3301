01a596e089f938b97179d7f3fce6a6daf02c76d3
Bogoslav-Gabriel BONAȚ <bonatbosa@gmail.com>
2020-11-15 20:57:40 +0200

    now yt is aded to gradle as well

--- app/src/main/java/com/example/guessmysong/YTSingleSong.java
+++ app/src/main/java/com/example/guessmysong/YTSingleSong.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.youtube.player.YouTubePlayerView;
+
 public class YTSingleSong extends AppCompatActivity {
+
+    private YouTubePlayerView yt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
