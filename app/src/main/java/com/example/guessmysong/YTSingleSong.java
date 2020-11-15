9239a90e684a13e60b7ba45d4275a673833277a1
Bogoslav-Gabriel BONAȚ <bonatbosa@gmail.com>
2020-11-15 22:33:56 +0200

    you can at least see the song

--- app/src/main/java/com/example/guessmysong/YTSingleSong.java
+++ app/src/main/java/com/example/guessmysong/YTSingleSong.java
@@ -2,17 +2,37 @@ package com.example.guessmysong;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
-public class YTSingleSong extends AppCompatActivity {
+public class YTSingleSong extends YouTubeBaseActivity {
 
-    private YouTubePlayerView yt;
+    private YouTubePlayerView mBouncySongYTPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_song);
+        mBouncySongYTPlayer = this.<YouTubePlayerView>findViewById(R.id.bouncySong);
+
+        YouTube.Search();
+        // prepare the listener
+        YouTubePlayer.OnInitializedListener onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("KGD2N5hJ2e0");
+                youTubePlayer.setManageAudioFocus(true);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        // initialize video
+        mBouncySongYTPlayer.initialize("AIzaSyBxzCAg00LCJFtVOPd-X0Rw6g_OeidRppQ", onInitializedListener);
     }
 }
